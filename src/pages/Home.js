44fdfab4d8581de196f8699ed24e2653cd6c3035
mahakchaich <mahakchaich105@gmail.com>
2022-04-06 00:15:11 +0100

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "../components/Form";
+import Header from "../components/Header";
+
+const Home=()=>{
+  return(
+    <div className="home-page">
+    <Header/>
+    <Form/>
+    </div>
+  );
+};
+export default Home;
